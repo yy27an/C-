0deeb7b8a192cd49ddcfd7049794b12de378a812
Neo <1297512143@qq.com>
2016-07-06 16:55:48 +0800

    Update 指针变量访问函数.c

--- C Examples/指针变量访问函数.c
+++ C Examples/指针变量访问函数.c
@@ -1,42 +1,42 @@
-//Ö¸Ïòº¯ÊıµÄÖ¸Õë±äÁ¿
-//#include <stdio.h>
-//int main(){
-//	int max(int,int);
-//	int (*p)(int,int);
-//	int a,b,c;
-//	p=max;			//º¯ÊımaxµÄÈë¿ÚµØÖ·
-//	printf("please enter a and b:");
-//	scanf("%d,%d",&a,&b);
-//	c=(*p)(a,b);
-//	printf("a=%d\nb=%d\nmax=%d\n",a,b,c);
-//	return 0;
-//}
-//
-//int max(int x, int y){
-//	int z;
-//	if(x>y) z=x;
-//	else z=y;
-//	return z;
-//}
+æŒ‡å‘å‡½æ•°çš„æŒ‡é’ˆå˜é‡
+#include <stdio.h>
+int main(){
+	int max(int,int);
+	int (*p)(int,int);
+	int a,b,c;
+	p=max;			//å‡½æ•°maxçš„å…¥å£åœ°å€
+	printf("please enter a and b:");
+	scanf("%d,%d",&a,&b);
+	c=(*p)(a,b);
+	printf("a=%d\nb=%d\nmax=%d\n",a,b,c);
+	return 0;
+}
 
-//#include <stdio.h>
-//int main(){
-//	int max(int,int);
-//	int min(int x,int y);
-//	int (*p)(int,int);
-//	int a,b,c,n;
-//	printf("please enter a and b:\n");
-//	scanf("%d,%d",&a,&b);
-//	printf("please choose 1 or 2\n:");
-//	scanf("%d",&n);
-//	if(n==1) p=max;
-//	else if(n==2) p=min;
-//	else printf("ÊäÈëÓĞÎó¡£");
-//	c=(*p)(a,b);
-//	printf("a=%d,b=%d\n",a,b);
-//	if(n==1) printf("max=%d\n",c);
-//	else printf("min=%d\n",c);
-//	return 0;
-//}
-//int max(int x,int y){return (x>y?x:y);}
-//int min(int x,int y){return (x<y?x:y);}
+int max(int x, int y){
+	int z;
+	if(x>y) z=x;
+	else z=y;
+	return z;
+}
+
+#include <stdio.h>
+int main(){
+	int max(int,int);
+	int min(int x,int y);
+	int (*p)(int,int);
+	int a,b,c,n;
+	printf("please enter a and b:\n");
+	scanf("%d,%d",&a,&b);
+	printf("please choose 1 or 2\n:");
+	scanf("%d",&n);
+	if(n==1) p=max;
+	else if(n==2) p=min;
+	else printf("è¾“å…¥æœ‰è¯¯ã€‚");
+	c=(*p)(a,b);
+	printf("a=%d,b=%d\n",a,b);
+	if(n==1) printf("max=%d\n",c);
+	else printf("min=%d\n",c);
+	return 0;
+}
+int max(int x,int y){return (x>y?x:y);}
+int min(int x,int y){return (x<y?x:y);}

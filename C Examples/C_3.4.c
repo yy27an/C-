f7833ee7ca63b827c49c3c785febe12d70e0ec2f
Neo <1297512143@qq.com>
2016-07-06 16:44:27 +0800

    Update C_3.4.c

--- C Examples/C_3.4.c
+++ C Examples/C_3.4.c
@@ -1,25 +1,25 @@
-//#include <stdio.h>
-//int main(){
-//	int f1=1,f2=1,f3;
-//	int i;
-//	printf("%12d\n%12d\n",f1,f2);
-//	for(i=1;i<=12;i++){
-//		f3=f1+f2;
-//		printf("%12d\n",f3);
-//		f1=f2;
-//		f2=f3;
-//	}
-//	return 0;
-//}
-//´ËÊ±Ö»ÐèÒªÁ½¸ö±äÁ¿¼´¿É
-//#include <stdio.h>
-//int main(){
-//	int f1=1,f2=1;
-//	int i;
-//	for(i=1;i<=20;i++){
-//		printf("%12d%12d",f1,f2);
-//		if(i%2==0) printf("\n");
-//		f1=f1+f2;
-//		f2=f2+f1;
-//	}
-//}
+#include <stdio.h>
+int main(){
+	int f1=1,f2=1,f3;
+	int i;
+	printf("%12d\n%12d\n",f1,f2);
+	for(i=1;i<=12;i++){
+		f3=f1+f2;
+		printf("%12d\n",f3);
+		f1=f2;
+		f2=f3;
+	}
+	return 0;
+}
+//æ­¤æ—¶åªéœ€è¦ä¸¤ä¸ªå˜é‡å³å¯
+#include <stdio.h>
+int main(){
+	int f1=1,f2=1;
+	int i;
+	for(i=1;i<=20;i++){
+		printf("%12d%12d",f1,f2);
+		if(i%2==0) printf("\n");
+		f1=f1+f2;
+		f2=f2+f1;
+	}
+}

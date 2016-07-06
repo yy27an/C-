d1924f424e4fc6bcd76c929dd75505f50eabcafa
Neo <1297512143@qq.com>
2016-07-06 16:50:46 +0800

    Update 建立内存动态分配区.c

--- C Examples/建立内存动态分配区.c
+++ C Examples/建立内存动态分配区.c
@@ -1,17 +1,17 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//int main(){
-//	void check(int *);
-//	int *p1,i;
-//	p1=(int *)malloc(5*sizeof(int));
-//	for(i=0;i<5;i++) scanf("%d",p1+i);
-//	check(p1);
-//	return 0;
-//}
-//void check(int *p){
-//	int i;
-//	printf("They are fail:\n");
-//	for(i=0;i<5;i++) if(p[i]<60) {printf("%d ",p[i]); printf("%d\n",p+i);}
-//	//while(p<=(p+5)) {if(*p<60) {printf("%d ",*p); printf("%d\n",p);} p++;}
-//	printf("\n");
-//}
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	void check(int *);
+	int *p1,i;
+	p1=(int *)malloc(5*sizeof(int));
+	for(i=0;i<5;i++) scanf("%d",p1+i);
+	check(p1);
+	return 0;
+}
+void check(int *p){
+	int i;
+	printf("They are fail:\n");
+	for(i=0;i<5;i++) if(p[i]<60) {printf("%d ",p[i]); printf("%d\n",p+i);}
+	//while(p<=(p+5)) {if(*p<60) {printf("%d ",*p); printf("%d\n",p);} p++;}
+	printf("\n");
+}

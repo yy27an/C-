b67513ae28e6cb941132cd1668814dcb9e9838fe
Neo <1297512143@qq.com>
2016-07-06 16:46:50 +0800

    Update 共用体类型.c

--- C Examples/共用体类型.c
+++ C Examples/共用体类型.c
@@ -1,28 +1,28 @@
-//#include <stdio.h>
-//struct{
-//	int num;
-//	char name[10];
-//	char sex;
-//	char job;
-//	union{
-//		int clas;				//�༶
-//		char position[10];		//ְ��
-//	}category;
-//}person[2];			//�ṹ������
-//int main(){
-//	int i;
-//	for(i=0;i<2;i++){
-//		printf("please enter the data of person:\n");
-//		scanf("%d %s %c %c",&person[i].num,&person[i].name,&person[i].sex,&person[i].job);
-//		if(person[i].job=='t') scanf("%s",&person[i].category.position);
-//		else if(person[i].job=='s') scanf("%d",&person[i].category.clas);
-//		else printf("Input error");
-//	}
-//	printf("\n");
-//	printf("No.  name        sex  job  class/position\n");
-//	for(i=0;i<2;i++){
-//		if(person[i].job=='s') printf("%-6d%-10s%-4c%-4c%-10d\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.clas);
-//		else printf("%-6d%-10s%-4c%-4c%-10d\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.position);
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+struct{
+	int num;
+	char name[10];
+	char sex;
+	char job;
+	union{
+		int clas;				//°à¼¶
+		char position[10];		//Ö°³Æ
+	}category;
+}person[2];			//½á¹¹ÌåÊý×é
+int main(){
+	int i;
+	for(i=0;i<2;i++){
+		printf("please enter the data of person:\n");
+		scanf("%d %s %c %c",&person[i].num,&person[i].name,&person[i].sex,&person[i].job);
+		if(person[i].job=='t') scanf("%s",&person[i].category.position);
+		else if(person[i].job=='s') scanf("%d",&person[i].category.clas);
+		else printf("Input error");
+	}
+	printf("\n");
+	printf("No.  name        sex  job  class/position\n");
+	for(i=0;i<2;i++){
+		if(person[i].job=='s') printf("%-6d%-10s%-4c%-4c%-10d\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.clas);
+		else printf("%-6d%-10s%-4c%-4c%-10d\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.position);
+	}
+	return 0;
+}

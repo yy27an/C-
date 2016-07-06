b7074621720d0cc2b79e1766b404d01349313cc1
Neo <1297512143@qq.com>
2016-07-06 16:47:34 +0800

    Update 可变格式输出函数.c

--- C Examples/可变格式输出函数.c
+++ C Examples/可变格式输出函数.c
@@ -1,11 +1,11 @@
-//#include <stdio.h>
-//int main(){
-//	int a=1,b=2;
-//	char *format="a=%d,b=%d\n";
+#include <stdio.h>
+int main(){
+	int a=1,b=2;
+	char *format="a=%d,b=%d\n";
+	printf(format,a,b);
+
+//	char format[]="a=%d,b=%d\n";
 //	printf(format,a,b);
-//
-////	char format[]="a=%d,b=%d\n";
-////	printf(format,a,b);
-//
-//	return 0;
-//}
+
+	return 0;
+}
